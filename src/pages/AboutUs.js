7c67ae309d971b88af2f97fcf486c2b6d3e47485
brianinq <tayloroovse@gmail.com>
2021-05-26 19:25:29 +0300

    Add Faqs section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {About} from '../components/About'
 import Services from '../components/Services'
+import Faqs from '../components/Faqs'
 
 export function AboutUs() {
     return(
         <>
             <About/>
             <Services/>
+            <Faqs/>
         </>
     )
 }
